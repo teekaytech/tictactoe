f733eb83d0fb9a9034f596b6c4e3034d98c29dd6
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-08-27 11:54:49 +0100

    add wining conditions

--- main.js
+++ main.js
@@ -1,64 +1,81 @@
-const parent = document.querySelector('.container');
-const cells = document.querySelectorAll('.cell');
+const parent = document.querySelector(".container");
+const cells = document.querySelectorAll(".cell");
 
 const Player = (name, mark) => {
-  return {name, mark}
-}
-
+  return { name, mark };
+};
 
 const GameBoard = () => {
-  moves = ['', '', '', '', '', '', '', '', ''];
+  moves = ["", "", "", "", "", "", "", "", ""];
 
   getMoves = () => {
     return moves;
+  };
+
+  clear = () => {};
+
+  return { getMoves, clear };
+};
+
+const Checkwin = (board, mark) => {
+  const ismatch = (val) => val === mark;
+  return board.every(ismatch);
+};
+const WinCond = (board, mark) => {
+  const con1 = Checkwin(board.slice(0, 3), mark);
+  const con2 = Checkwin(board.slice(3, 6), mark);
+  const con3 = Checkwin(board.slice(5, 9), mark);
+  const con4 = Checkwin([board[0], board[3], board[6]], mark);
+  const con5 = Checkwin([board[1], board[4], board[7]], mark);
+  const con6 = Checkwin([board[2], board[5], board[8]], mark);
+  const con7 = Checkwin([board[0], board[4], board[8]], mark);
+  const con8 = Checkwin([board[2], board[4], board[6]], mark);
+  function checktrue(index) {
+    return index === true;
   }
-
-  clear = () => {
-
-  }
-
-  update = () => {
-
-  }
-
-  return {getMoves, clear, update}
-}
+  return [con1, con2, con3, con4, con5, con6, con7, con8].some(checktrue);
+};
 
 const gameController = (board) => {
   render = () => {
     for (let index = 0; index < cells.length; index++) {
       cells[index].textContent = board[index];
     }
-  }
+  };
 
   checkMove = (cell) => {
-    return cell.innerHTML = '' ? false : true
-  }
+    return (cell.innerHTML = "" ? false : true);
+  };
 
   makeMove = (cell, mark) => {
-    if (board[cell.id] === '') {
+    if (board[cell.id] === "") {
       board[cell.id] = mark;
       console.log(board);
       render();
       return true;
-    } else { return false; }
-  }
+    } else {
+      return false;
+    }
+  };
 
   validateMove = (cells, move) => {
+    let gameon = true;
     for (const cell of cells) {
-      cell.addEventListener('click', function() {
-        return makeMove(cell, move) ? true : false;
+      cell.addEventListener("click", function () {
+        makeMove(cell, move) ? true : false;
+        if (WinCond(board, player1.mark)) {
+          console.log("game over");
+          return (gameon = false);
+        }
       });
     }
-  }
+  };
 
-  return {render, validateMove}
-}
-
-
-const player1 = Player('player1', 'X');
-const player2 = Player('player2', 'O');
+  return { render, validateMove };
+};
+const player1 = Player("player1", "X");
+const player2 = Player("player2", "O");
 const board = GameBoard();
 const game = gameController(board.getMoves());
 game.render();
-game.validateMove(cells, 'A');
+game.validateMove(cells, player1.mark);
