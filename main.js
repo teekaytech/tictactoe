e85c0ddce54723738bd785a4460ebc800f8061d5
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-09-04 15:47:36 +0100

    add score and names to diplay on the frontend

--- main.js
+++ main.js
@@ -2,27 +2,22 @@ const mainContainer = document.querySelector(".container");
 let cells = document.querySelectorAll(".cell");
 const tictactoebord = document.querySelector(".board");
 const movehtml = document.querySelector(".move");
+const form = document.querySelector("#form");
+const player1_score = document.querySelector("#player1_score");
+const player2_score = document.querySelector("#player2_score");
 
-const Player = (name, mark) => {
-  return { name, mark };
+const Player = (name, mark, score) => {
+  return { name, mark, score };
 };
 
 const GameBoard = () => {
   let moves = ["", "", "", "", "", "", "", "", ""];
 
-  getMoves = () => {
-    return moves;
-  };
-
   showBoard = () => {
-    return (tictactoebord.style.display = "block");
+    return (tictactoebord.style.display = "inline-block");
   };
 
-  clear = () => {
-    return (moves = ["", "", "", "", "", "", "", "", ""]);
-  };
-
-  return { getMoves, clear, showBoard };
+  return { moves, showBoard };
 };
 
 const Checkwin = (board, mark) => {
@@ -32,7 +27,7 @@ const Checkwin = (board, mark) => {
 const WinCond = (board, mark) => {
   const con1 = Checkwin(board.slice(0, 3), mark);
   const con2 = Checkwin(board.slice(3, 6), mark);
-  const con3 = Checkwin(board.slice(5, 9), mark);
+  const con3 = Checkwin(board.slice(6, 9), mark);
   const con4 = Checkwin([board[0], board[3], board[6]], mark);
   const con5 = Checkwin([board[1], board[4], board[7]], mark);
   const con6 = Checkwin([board[2], board[5], board[8]], mark);
@@ -62,25 +57,35 @@ const gameController = (board) => {
     }
   };
 
-  validateMove = (cells, markstart) => {
-    let mark = markstart;
+  validateMove = (cells, players) => {
+    let player = 0;
     cells.forEach((cell) => {
       cell.addEventListener("click", function () {
         console.log(cell.id);
         if (cell.innerHTML.length == 0) {
           console.log(cell.id);
-          board = makeMove(cell, mark);
-          if (WinCond(board, mark)) {
-            movehtml.innerHTML = "Game Over!!!";
+          board = makeMove(cell, players[player].mark);
+          if (WinCond(board, players[player].mark)) {
+            movehtml.innerHTML = `Game Over!!! ${players[player].name} Won`;
             mainContainer.appendChild(restart);
             tictactoebord.style.display = "none";
             board = ["", "", "", "", "", "", "", "", ""];
-          } else if (!WinCond(board, mark) && board.includes("")) {
-            mark === "X" ? (mark = "O") : (mark = "X");
-            console.log(mark);
-            movehtml.innerHTML = "change turn";
+            players[player].score += 1;
+            showScore(players);
+            console.log(players[player].score);
+            restart.style.display = "inline-block";
+          } else if (
+            !WinCond(board, players[player].mark) &&
+            board.includes("")
+          ) {
+            player === 0 ? (player = 1) : (player = 0);
+            console.log(players[player].mark);
+            movehtml.innerHTML = `now it ${players[player].name} turn`;
           } else {
-            console.log("draw");
+            tictactoebord.style.display = "none";
+            movehtml.innerHTML = " it a draw better luck next match!";
+            mainContainer.appendChild(restart);
+            restart.style.display = "inline-block";
           }
         } else {
           movehtml.innerHTML = "Invalid move";
@@ -91,16 +96,10 @@ const gameController = (board) => {
 
   return { render, validateMove };
 };
-
-const gameInit = (board, player) => {
-  let game = gameController(board.getMoves());
-  board.showBoard();
-  game.render();
-  game.validateMove(cells, player.mark);
-
-  return {};
+const showScore = (players_lis) => {
+  player1_score.innerHTML = `${players_lis[0].name} score : ${players_lis[0].score}`;
+  player2_score.innerHTML = `${players_lis[1].name} score : ${players_lis[1].score}`;
 };
-
 const players = document.querySelectorAll(".player");
 const startGame = document.getElementById("start");
 const restart = document.createElement("button");
@@ -110,25 +109,28 @@ restart.textContent = "Restart";
 
 startGame.addEventListener("click", function (e) {
   e.preventDefault();
-
+  let players_lis = [];
   let board = GameBoard();
-  let player1 = Player(players[0].value, "X");
-  let player2 = Player(players[1].value, "O");
-
-  gameInit(board, player1);
+  players_lis.push(Player(players[0].value, "X", 0));
+  players_lis.push(Player(players[1].value, "O", 0));
+  movehtml.innerHTML = `hello players , ${players_lis[0].name} will play with ${players_lis[0].mark} 
+  and ${players_lis[1].name} will play with ${players_lis[1].mark}`;
+  let game = gameController(board.moves);
+  board.showBoard();
+  game.render();
+  game.validateMove(cells, players_lis);
+  form.style.display = "none";
+  showScore(players_lis);
 });
 
 restart.addEventListener("click", function (e) {
   e.preventDefault();
-  // board = GameBoard();
-
-  // console.log(board.clear());
-  cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
     cell.innerHTML = "";
     cell.innerText = "";
   });
   let board = GameBoard();
-  let game = gameController(board.clear());
   board.showBoard();
+  movehtml.innerHTML = "let start again";
+  restart.style.display = "none";
 });
